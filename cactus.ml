061053915d1423e72d9ee92f6f910a89c664bbad
Virgile Robles <virgile.robles@pm.me>
2021-01-01 22:48:59 +0100

    Allow aiming for a giving temperature

--- cactus.ml
+++ cactus.ml
@@ -1,21 +1,69 @@
-let period = 2. *. 60. (* seconds *)
-let active_duty = 0.20 (* [0 - 1] *)
+type pwm_config = {
+  period : float; (* seconds *)
+  duty : float; (* [0 - 1] *)
+}
 
-let rec active_loop () =
-  let active_length = active_duty *. period in
-  let standby_length = (1. -. active_duty) *. period in
-  Io.select Mode.Active;
-  Io.sleep ~blink_mode:Mode.Idle active_length;
-  Io.select Mode.Idle;
-  Io.sleep ~blink_mode:Mode.Disabled standby_length;
-  active_loop ()
+let active_pwm = {period = 60.; duty = 0.8}
+
+let show_temperature t =
+  Printf.printf "Current temperature: %f°C\n" t
+
+let rec active_loop pwm times =
+  if times > 0 then begin
+    let active_length = pwm.duty *. pwm.period in
+    let standby_length = (1. -. pwm.duty) *. pwm.period in
+    Io.select Mode.Active;
+    Io.sleep ~blink_mode:Mode.Idle active_length;
+    Io.select Mode.Idle;
+    Io.sleep ~blink_mode:Mode.Idle standby_length;
+    active_loop pwm (times - 1)
+  end
+
+let margin = 1.0 (* margin below goal temperature *)
+
+let rec heat_goal t goal = 
+  let current = Temperature.get t in
+  show_temperature current;
+  if current >= goal then
+    wait_goal t goal
+  else begin
+    active_loop active_pwm 1;
+    heat_goal t goal
+  end
+
+and wait_goal t goal =
+  let current = Temperature.get t in
+  show_temperature current;
+  if current <= (goal -. margin) then
+    heat_goal t goal
+  else begin
+    Io.select Mode.Idle;
+    Io.sleep ~blink_mode:Mode.Disabled 60.;
+    wait_goal t goal
+  end
+
+let usage () =
+  Printf.printf "Usage: cactus select [on|off]\n";
+  Printf.printf "       cactus goal TEMPERATURE\n";
+  Printf.printf "       cactus read\n"
 
 let main =
   Io.init ();
-  let t = Temperature.init () in
-  Printf.printf "Current temperature: %f\n" (Temperature.get t);
-  if ((Array.length Sys.argv) <> 2) then (
-    Printf.printf "Auto mode enabled\n%!";
-    active_loop ()
-  ) else
-    Sys.argv.(1) |> Mode.of_string |> Io.select
+  let argc = Array.length Sys.argv in
+  if argc >= 2 then
+    match Sys.argv.(1) with
+    | "select" ->
+      if argc <> 3 then usage () else
+        Sys.argv.(2) |> Mode.of_string |> Io.select
+    | "goal" ->
+      if argc <> 3 then usage () else
+        begin match float_of_string_opt Sys.argv.(2) with
+          | None -> failwith "Temperature must be a float"
+          | Some goal ->
+            let driver = Temperature.init () in
+            heat_goal driver goal
+        end
+    | "read" ->
+      Temperature.init () |> Temperature.get |> show_temperature
+    | _ -> failwith "Unknown command"
+  else usage()
